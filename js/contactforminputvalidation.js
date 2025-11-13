d16e39107eaa0d6b933d9e1f2ad7351412bc3f3e
X25145347 <X25145347@student.ncirl.ie>
2025-11-13 22:39:45 +0000

    Contact form update
    Contact form update

---
+++ js/contactforminputvalidation.js
@@ -0,0 +1,43 @@
+//Created by Roisin Kelly
+let contactFormEvent = document.getElementById("contactForm");
+contactFormEvent.addEventListener("submit", function(event) {
+  event.preventDefault();
+ 
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let phone = document.getElementById("phone").value.trim();
+  let feedbackError = document.getElementById("feedbackModalBodyError");
+  let headerError = document.getElementById("headerError");
+  let errorModal = document.getElementById("errorModal");
+  let successModal = document.getElementById("successModal");
+  if (name === "" ) {
+	  headerError.innerHTML = "Invalid name!";
+	  feedbackError.innerHTML="Please enter name!";
+	  successModal.classList.add("inVisibleDisplay");
+    return;
+  }
+  else if(isNaN(phone) || phone === ""){
+	headerError.innerHTML = "Invalid phone number!";
+    feedbackError.innerHTML="Please enter valid phone number!";
+	successModal.classList.add("inVisibleDisplay");
+	return;
+  }
+  else if (email === "") {
+	headerError.innerHTML = "Invalid email!";
+    feedbackError.innerHTML="Please enter valid email!";
+	successModal.classList.add("inVisibleDisplay");
+    return;
+  }
+  else{
+	  let feedbackSuccess = document.getElementById("feedbackModalBodySSuccess");
+	  feedbackSuccess.innerHTML= "Hi "+name+", thank you for your details. We will be in touch via "+phone+" shortly."
+	  let formTitle = document.getElementById("title");
+	 formTitle.innerHTML = "Thank You";
+	  errorModal.classList.add("inVisibleDisplay");
+	  contactForm.classList.add("inVisibleDisplay");
+	  successModal.classList.remove("inVisibleDisplay");
+	  return;
+  }
+  
+});
+
