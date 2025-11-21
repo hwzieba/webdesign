b9127331f3b3b091e7649f34fb334ad067489544
hwzieba <48774964+hwzieba@users.noreply.github.com>
2025-11-21 18:52:50 +0000

    Changing the layout
    TODO:
    -column width on the contact page
    -button colors on the contact page

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,15 @@
-let slideIndex = 0;
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 6000); // Change image every 2 seconds
+  var slides = document.getElementsByClassName("slides");
+  for (i=0;i<slides.length;i++){
+    slides[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length){
+    slideIndex = 1
+  }
+  slides[slideIndex-1].classList.add("active");
+  setTimeout(showSlides, 5000);
 }
