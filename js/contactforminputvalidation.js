08fe8873f29785cc7c2e5abc5320a56059f58d2b
X25145347 <X25145347@student.ncirl.ie>
2025-12-03 18:43:50 -0500

    Layout fix
    Fixing a layout issue when created when adding encoded empty space in empty tags.

--- js/contactforminputvalidation.js
+++ js/contactforminputvalidation.js
@@ -35,6 +35,7 @@ contactFormEvent.addEventListener("submit", function(event) {
 	formTitle.innerHTML = "Thank You";
 	errorModal.classList.add("inVisibleDisplay");
 	contactForm.classList.add("inVisibleDisplay");
+	formTitle.classList.remove("inVisibleDisplay");
 	contactMessage.innerHTML = "&nbsp;";
 	successModal.classList.remove("inVisibleDisplay");
 	return;
