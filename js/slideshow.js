a0d43f041f8fd8932d537ffc361fddda8a313d06
hwzieba <48774964+hwzieba@users.noreply.github.com>
2025-12-07 00:28:55 +0000

    Adjusting css an js

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,13 +3,16 @@ showSlides();
 
 function showSlides() {
   var slides = document.getElementsByClassName("slides");
+  //Hiding the slides
   for (i=0;i<slides.length;i++){
     slides[i].classList.remove("active");
   }
   slideIndex++;
+  //Resetting the index
   if (slideIndex > slides.length){
     slideIndex = 1
   }
+  //Showing one slide
   slides[slideIndex-1].classList.add("active");
   setTimeout(showSlides, 5000);
 }
